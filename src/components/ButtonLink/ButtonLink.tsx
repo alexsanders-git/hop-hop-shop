b1c91878d05543c7b4f9668e52236ade5ce61a63
alex <alex.sanders@ukr.net>
2024-06-25 18:45:04 +0300

    Update 404 page

--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -5,11 +5,21 @@ import styles from './ButtonLink.module.scss';
 interface IButtonLinkProps {
   href: string;
   text: string;
+  className?: string;
+  style?: 'primary' | 'secondary';
 }
 
-export default function ButtonLink({ href, text }: IButtonLinkProps) {
+export default function ButtonLink({
+  href,
+  text,
+  className = '',
+  style = 'primary'
+}: IButtonLinkProps) {
   return (
-    <Link href={href} className={styles.button}>
+    <Link
+      href={href}
+      className={`${styles.button} ${className} ${style === 'secondary' ? styles.secondary : styles.primary}`}
+    >
       {text}
     </Link>
   );
