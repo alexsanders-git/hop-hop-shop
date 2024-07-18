351d27bea5f4aec979954b1f8ca3933731a68f9d
ihor <ihorvitrikush0101@gmail.com>
2024-07-18 11:52:38 +0300

    fixed bugs part 1

--- src/components/dashboard/DashboardSidebar/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar/DashboardSidebar.tsx
@@ -1,17 +1,25 @@
 'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { sidebarLinks } from '@/utils/linksData/linksData';
 
 import styles from './styles.module.scss';
 
 export default function DashboardSidebar() {
+	const pathname = usePathname();
+	const isActive = (path: string) => path === pathname;
 	return (
 		<aside className={styles.sidebar}>
 			<ul>
 				{sidebarLinks.map((link, index) => (
 					<li key={index}>
-						<Link href={link.path}>{link.name}</Link>
+						<Link
+							className={`${styles.link} ${isActive(link.path) ? styles.active : ''}`}
+							href={link.path}
+						>
+							{link.name}
+						</Link>
 					</li>
 				))}
 			</ul>
