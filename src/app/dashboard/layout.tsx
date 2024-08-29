a181b46aa10b70a70665145488f7b942deaa9bcf
alex <alex.sanders@ukr.net>
2024-08-29 18:33:11 +0300

    Add clarity tag

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 import Container from '@/components/Container/Container';
 import DashboardFooter from '@/components/dashboard/dashboardFooter/DashboardFooter';
@@ -33,6 +34,16 @@ export default function DashboardLayout({
 						<DashboardFooter />
 					</Container>
 				</div>
+
+				<Script id="clarity-script" strategy="afterInteractive">
+					{`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+				</Script>
 			</body>
 		</html>
 	);
