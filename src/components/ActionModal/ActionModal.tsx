edbcdeb1a3bf77bc573e5eed4f6b36d4d7c8730e
oksana <oksana.koss.it@gmail.com>
2024-09-05 06:37:04 +0300

    unsuccess modal added

--- src/components/ActionModal/ActionModal.tsx
+++ src/components/ActionModal/ActionModal.tsx
@@ -3,48 +3,59 @@ import { forwardRef, Ref } from 'react';
 
 import { robotoCondensed } from '@/styles/fonts/fonts';
 
-import styles from './SuccessActionModal.module.scss';
+import styles from './ActionModal.module.scss';
 import ButtonLink from '../ButtonLink/ButtonLink';
 
 interface ModalProps {
 	show: boolean;
 	title: string;
 	text: string;
+	type: 'success' | 'error';
 	onClose: () => void;
 }
 
-const SuccessActionModal = forwardRef(function SuccessActionModal(
-	{ show, title, text, onClose }: ModalProps,
+const ActionModal = forwardRef(function ActionModal(
+	{ show, title, text, type, onClose }: ModalProps,
 	ref: Ref<HTMLDivElement>,
 ) {
 	if (!show) return null;
 
+	const iconSrc =
+		type === 'success'
+			? '/thanksPageIllustration.svg'
+			: '/unSuccessIllustration_wrong.svg';
+
 	return (
 		<div className={styles.backdrop}>
-			<div className={styles.modalBody} ref={ref}>
+			<div
+				className={`${styles.modalBody} ${type === 'error' ? styles.error : ''}`}
+				ref={ref}
+			>
 				<div className={styles.textPart}>
 					<h2 className={styles.title}>{title}</h2>
 					<p className={`${styles.subtitle} ${robotoCondensed.className}`}>
 						{text}
 					</p>
 				</div>
-				<div className={styles.imageWrapper}>
+				<div
+					className={`${styles.imageWrapper} ${type === 'error' ? styles.error : ''}`}
+				>
 					<Image
-						src={'/thanksPageIllustration.svg'}
+						src={iconSrc}
 						width={121}
 						height={200}
-						alt={'like'}
+						alt={type === 'success' ? 'like' : 'error'}
 					/>
 				</div>
 				<ButtonLink
 					href={'/'}
 					text={'Back to shopping!'}
-					className={styles.closeButton}
+					className={`${styles.closeButton} ${type === 'error' ? styles.error : ''}`}
 				></ButtonLink>
 			</div>
 		</div>
 	);
 });
 
-SuccessActionModal.displayName = 'SuccessActionModal';
-export default SuccessActionModal;
+ActionModal.displayName = 'SuccessActionModal';
+export default ActionModal;
