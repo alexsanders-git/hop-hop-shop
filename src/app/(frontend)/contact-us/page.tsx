27672e8187f94159644c7b907cf3fd8d210f25b5
oksana <oksana.koss.it@gmail.com>
2024-09-18 12:40:40 +0300

    Added contact-us page left part

---
+++ src/app/(frontend)/contact-us/page.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import ContactUsForm from './ContactUsForm/ContactUsForm';
+
+import styles from './page.module.scss';
+
+export default function ContactUsPage() {
+	return (
+		<div className={styles.pageWrapper}>
+			<div className={styles.left}>
+				<h1 className={styles.title}>Got Questions?</h1>
+				<div className={styles.imageWrapper}>
+					<Image
+						src={'/illustration_admin.svg'}
+						width={391}
+						height={391}
+						alt="admin"
+					/>
+				</div>
+				<div className={styles.text}>
+					<p className={styles.paragraph}>You've found the right place!</p>
+					<p className={styles.paragraph}>
+						Whether you need help, a virtual high-five, or just want to chat
+						about the weather, we're all ears
+					</p>
+				</div>
+			</div>
+			<ContactUsForm />
+		</div>
+	);
+}
