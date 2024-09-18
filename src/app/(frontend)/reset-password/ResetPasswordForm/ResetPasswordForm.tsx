d40fe025ac32bb9936bdb6c1ff635cc0e79455e0
alex <alex.sanders@ukr.net>
2024-09-18 10:25:38 +0300

    lint

--- src/app/(frontend)/reset-password/ResetPasswordForm/ResetPasswordForm.tsx
+++ src/app/(frontend)/reset-password/ResetPasswordForm/ResetPasswordForm.tsx
@@ -26,7 +26,6 @@ export default function ResetPasswordForm() {
 	const token = searchParams.get('key') || '';
 	const user_email = searchParams.get('email') || '';
 
-
 	const {
 		ref: successModalRef,
 		isShow: isSuccessModalOpen,
