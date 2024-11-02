ea0e1813e84e29c371395812871b6a11b90dd661
alex <alex.sanders@ukr.net>
2024-11-02 17:02:28 +0200

    Update test page

--- src/app/(frontend)/test-page/page.tsx
+++ src/app/(frontend)/test-page/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+
+import SectionContainer from '@/components/SectionContainer/SectionContainer';
+
 import styles from './styles.module.scss';
 
 export default function Test() {
-	return <div className={styles.container}></div>;
+	const onClickHandler = () => {
+		console.log('test');
+	};
+
+	return (
+		<SectionContainer>
+			<button onClick={() => onClickHandler()}>TEST</button>
+		</SectionContainer>
+	);
 }
