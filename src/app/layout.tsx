0914bd5adfadc51e5e8d1e180a9c663b9c37bfdb
alex <alex.sanders@ukr.net>
2024-11-22 21:28:26 +0200

    Add Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,20 +15,34 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en">
-			<body className={londrinaSolid.className}>
-				<div className={styles.bodyInner}>{children}</div>
+			<head>
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-5RZYXBRX4N"
+				/>
 
-				<Initializer />
+				<Script id="gtag">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-5RZYXBRX4N');
+					`}
+				</Script>
 
 				<Script id="clarity-script" strategy="afterInteractive">
 					{`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-          `}
+						(function(c,l,a,r,i,t,y){
+							c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+							t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+							y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+						})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+					`}
 				</Script>
+			</head>
+			<body className={londrinaSolid.className}>
+				<div className={styles.bodyInner}>{children}</div>
+				<Initializer />
 			</body>
 		</html>
 	);
